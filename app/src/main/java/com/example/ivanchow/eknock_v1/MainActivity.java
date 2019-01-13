2f4d9cbe17f0e8fb8e96e77ae3701057da39cf7d
jukeboxhero123 <jerrytan7@gmail.com>
2019-01-12 20:36:42 -0500

    YIBM tone analyzer

--- app/src/main/java/com/example/ivanchow/eknock_v1/MainActivity.java
+++ app/src/main/java/com/example/ivanchow/eknock_v1/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.ivanchow.eknock_v1;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -15,13 +22,15 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     TextView resultText;
-    private String input;
+    private String inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         resultText  = (TextView)findViewById(R.id.textViewResult);
     }
 
@@ -30,7 +39,25 @@ public class MainActivity extends AppCompatActivity {
             promptSpeechInput();
         }
     }
+    public ToneAnalysis getTone(String bam){
+        IamOptions options = new IamOptions.Builder()
+                .apiKey("rlJ4zjrOU9mXG1SQw8ekGWyHmN7XfNSytFM_-cCpugl4")
+                .build();
 
+        ToneAnalyzer toneAnalyzer = new ToneAnalyzer("2017-09-21", options);
+        toneAnalyzer.setEndPoint("https://gateway.watsonplatform.net/tone-analyzer/api");
+
+        String text = bam;
+
+        ToneOptions toneOptions = new ToneOptions.Builder()
+                .text(text)
+                .build();
+
+        ToneAnalysis toneAnalysis = toneAnalyzer.tone(toneOptions).execute();
+        //for()
+        Log.w("tone", toneAnalysis.toString());
+        return toneAnalysis;
+    }
     public void promptSpeechInput(){
         Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -52,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
             case 100: if(result_code == RESULT_OK && i != null){
                 ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 resultText.setText(result.get(0));
-                input = resultText.getText().toString();
+                getTone(result.get(0));
             }
                 break;
         }
